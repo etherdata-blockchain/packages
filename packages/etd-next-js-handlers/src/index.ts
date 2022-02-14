ed4a07c0bf5e01b78c15337c35a1f4f73e3a8308
sirily11 <32106111+sirily11@users.noreply.github.com>
2022-02-14 15:18:45 +0800

    feat: add device authorization handler (#22)

--- packages/etd-next-js-handlers/src/index.ts
+++ packages/etd-next-js-handlers/src/index.ts
@@ -2,6 +2,7 @@ import { jwtVerificationQueryHandler } from "./handlers/jwt_verification_query_h
 import { jwtVerificationHandler } from "./handlers/jwt_verification_handler";
 import { methodAllowedHandler } from "./handlers/method_allowed_handler";
 import { paginationHandler } from "./handlers/pagination_handler";
+import { deviceAuthorizationHandler } from "./handlers/device_authorization_handler";
 
 export {
   jwtVerificationQueryHandler,
