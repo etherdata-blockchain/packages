fe1c5fd426e8232ca5b04e830eae1fc8a419939c
sirily11 <32106111+sirily11@users.noreply.github.com>
2022-02-14 14:03:18 +0800

    feat: add maximum retries limit to the service (#21)
    * feat: add maximum retries limit to the service

--- packages/etd-services/src/mongodb/services/job/job_result_service.ts
+++ packages/etd-services/src/mongodb/services/job/job_result_service.ts
@@ -1,8 +1,9 @@
 import { schema } from "@etherdata-blockchain/storage-model";
-import { enums } from "@etherdata-blockchain/common";
+import { enums, interfaces, configs } from "@etherdata-blockchain/common";
 import { Model } from "mongoose";
 import { BaseMongoDBService } from "../../db_service";
 import moment from "moment";
+import { PendingJobService } from "./pending_job_service";
 
 // eslint-disable-next-line require-jsdoc
 export class JobResultService extends BaseMongoDBService<schema.IJobResult> {
@@ -30,5 +31,30 @@ export class JobResultService extends BaseMongoDBService<schema.IJobResult> {
   async removeOutdatedJobs(maximumDuration: number) {
     const deadline = moment().subtract(maximumDuration, "seconds");
     await this.model.deleteMany({ time: { $lte: deadline.toDate() } });
+  }
+
+  async submitResult(
+    result: interfaces.db.JobResultDBInterface,
+    pendingJob: interfaces.db.PendingJobDBInterface<any>
+  ): Promise<{ success: boolean; reason?: string }> {
+    try {
+      const pendingJobService = new PendingJobService();
+      if (pendingJob.tries > configs.Configurations.maximumRetiresAllowed - 1) {
+        await pendingJobService.delete((pendingJob as any)._id);
+        return { success: false, reason: "Tries exceeds the limit" };
+      }
+      if (!result.success) {
+        // Delete the pending job if it exceeds the limit
+        pendingJob.tries = pendingJob.tries + 1;
+        await pendingJobService.patch(pendingJob as any);
+        return { success: true };
+      } else {
+        await pendingJobService.delete((pendingJob as any)._id);
+      }
+      await this.patch(result as any);
+      return { success: true };
+    } catch (e) {
+      return { success: false, reason: `${e}` };
+    }
   }
 }
