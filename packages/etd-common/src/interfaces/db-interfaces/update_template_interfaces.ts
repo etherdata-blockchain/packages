b0e985ea163fbe2b312a1bfa762571d686b1268c
sirily11 <sirily1997@gmail.com>
2022-02-24 16:55:17 +0800

    fix: add HostConfig

--- packages/etd-common/src/interfaces/db-interfaces/update_template_interfaces.ts
+++ packages/etd-common/src/interfaces/db-interfaces/update_template_interfaces.ts
@@ -1,3 +1,5 @@
+import { DeviceRequest, MountConfig } from "dockerode";
+
 /**
  * Stack to describe the structure of the container
  */
@@ -12,6 +14,76 @@ export interface ImageStack {
   imageId?: string;
   image: string;
   tag: string;
+}
+
+interface HostConfig {
+  AutoRemove?: boolean | undefined;
+  Binds?: string[] | undefined;
+  ContainerIDFile?: string | undefined;
+  LogConfig?:
+    | {
+        Type: string;
+        Config: any;
+      }
+    | undefined;
+  NetworkMode?: string | undefined;
+  PortBindings?: any;
+  RestartPolicy?: any | undefined;
+  VolumeDriver?: string | undefined;
+  VolumesFrom?: any;
+  Mounts?: MountConfig | undefined;
+  CapAdd?: any;
+  CapDrop?: any;
+  Dns?: any[] | undefined;
+  DnsOptions?: any[] | undefined;
+  DnsSearch?: any[] | undefined;
+  ExtraHosts?: any;
+  GroupAdd?: string[] | undefined;
+  IpcMode?: string | undefined;
+  Cgroup?: string | undefined;
+  Links?: any;
+  OomScoreAdj?: number | undefined;
+  PidMode?: string | undefined;
+  Privileged?: boolean | undefined;
+  PublishAllPorts?: boolean | undefined;
+  ReadonlyRootfs?: boolean | undefined;
+  SecurityOpt?: any;
+  StorageOpt?: { [option: string]: string } | undefined;
+  Tmpfs?: { [dir: string]: string } | undefined;
+  UTSMode?: string | undefined;
+  UsernsMode?: string | undefined;
+  ShmSize?: number | undefined;
+  Sysctls?: { [index: string]: string } | undefined;
+  Runtime?: string | undefined;
+  ConsoleSize?: number[] | undefined;
+  Isolation?: string | undefined;
+  MaskedPaths?: string[] | undefined;
+  ReadonlyPaths?: string[] | undefined;
+  CpuShares?: number | undefined;
+  CgroupParent?: string | undefined;
+  BlkioWeight?: number | undefined;
+  BlkioWeightDevice?: any;
+  BlkioDeviceReadBps?: any;
+  BlkioDeviceWriteBps?: any;
+  BlkioDeviceReadIOps?: any;
+  BlkioDeviceWriteIOps?: any;
+  CpuPeriod?: number | undefined;
+  CpuQuota?: number | undefined;
+  CpusetCpus?: string | undefined;
+  CpusetMems?: string | undefined;
+  Devices?: any;
+  DeviceCgroupRules?: string[] | undefined;
+  DeviceRequests?: DeviceRequest[] | undefined;
+  DiskQuota?: number | undefined;
+  KernelMemory?: number | undefined;
+  Memory?: number | undefined;
+  MemoryReservation?: number | undefined;
+  MemorySwap?: number | undefined;
+  MemorySwappiness?: number | undefined;
+  OomKillDisable?: boolean | undefined;
+  Init?: boolean | undefined;
+  PidsLimit?: number | undefined;
+  Ulimits?: any;
 }
 
 export interface DockerContainerConfig {
@@ -35,7 +107,7 @@ export interface DockerContainerConfig {
   ExposedPorts?: { [port: string]: {} } | undefined;
   StopSignal?: string | undefined;
   StopTimeout?: number | undefined;
-  HostConfig?: any | undefined;
+  HostConfig?: HostConfig | undefined;
   NetworkingConfig?:
     | {
         EndpointsConfig?: any | undefined;
