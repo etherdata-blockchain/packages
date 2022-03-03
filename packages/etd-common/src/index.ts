2e6ffbab69046361d1f80a642bdddb3ce7393efa
sirily11 <32106111+sirily11@users.noreply.github.com>
2022-03-03 22:14:35 +0800

    feat: export json schema for db interfaces (#40)

--- packages/etd-common/src/index.ts
+++ packages/etd-common/src/index.ts
@@ -3,3 +3,4 @@ export * as enums from "./enums";
 export * as configs from "./configs";
 export * as utils from "./utils";
 export * as mockData from "./mockdata";
+export * as schema from "./schemas";
