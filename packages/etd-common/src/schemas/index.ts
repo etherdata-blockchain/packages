ac90431e05ea061397310e883d190eabe5a0e772
sirily11 <sirily1997@gmail.com>
2022-03-03 21:14:47 +0800

    feat: export json schema for db interfaces

---
+++ packages/etd-common/src/schemas/index.ts
@@ -0,0 +1,27 @@
+import deviceSchema from "./device_interfaces.json";
+import dockerSchema from "./docker_interfaces.json";
+import etdSchema from "./etd_interfaces.json";
+import executionPlanSchema from "./execution_plan_interfaces.json";
+import installationTemplateSchema from "./execution_plan_interfaces.json";
+import jobResultSchema from "./job_result_interfaces.json";
+import pendingJobSchema from "./pending_job_interfaces.json";
+import pluginInterfaceSchema from "./plugin_interfaces.json";
+import requestSchema from "./request_interfaces.json";
+import staticNodeSchema from "./static_node_interfaces.json";
+import storageInterfaceSchema from "./storage_interfaces.json";
+import transactionSchema from "./transaction_interfaces.json";
+
+export {
+  deviceSchema,
+  dockerSchema,
+  executionPlanSchema,
+  jobResultSchema,
+  pendingJobSchema,
+  requestSchema,
+  pluginInterfaceSchema,
+  storageInterfaceSchema,
+  installationTemplateSchema,
+  staticNodeSchema,
+  transactionSchema,
+  etdSchema,
+};
