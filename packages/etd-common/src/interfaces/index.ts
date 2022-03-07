d7a0f57a9551de60fbaa4518aed663ca76fe9569
sirily11 <32106111+sirily11@users.noreply.github.com>
2022-03-07 18:57:24 +0800

    fix: merge two docker interfaces (#41)

--- packages/etd-common/src/interfaces/index.ts
+++ packages/etd-common/src/interfaces/index.ts
@@ -2,4 +2,3 @@ export * as db from "./db-interfaces";
 export * from "./request_interfaces";
 export * from "./plugin_interfaces";
 export * from "./etd_interfaces";
-export * from "./docker_interfaces";
