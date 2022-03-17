246fe8a836d519fb9af49122c18f8a1c4acb2acc
Han Bo <lingwu_hb@163.com>
2022-03-17 20:18:04 +0800

    fix: add OwnerGroupSchema

--- packages/etd-storage-model/src/db-schema/device/storage/owner.ts
+++ packages/etd-storage-model/src/db-schema/device/storage/owner.ts
@@ -5,24 +5,37 @@ import mongoose, { Document, model, Model, Schema } from "mongoose";
 import { enums, interfaces } from "@etherdata-blockchain/common";
 
 export interface IStorageOwner
-  extends Document,
-    interfaces.db.StorageUserDBInterface {
-  onlineCount?: number;
-  totalCount?: number;
+    extends Document,
+        interfaces.db.StorageUserDBInterface {
+    onlineCount?: number;
+    totalCount?: number;
+}
+
+export interface IStorageOwnerGroup
+    extends Document,
+        IStorageOwner {
+    group_id: string;
 }
 
 export const storageOwnerSchema = new Schema<IStorageOwner>(
-  {
-    user_id: { type: "string", index: true, unique: true },
-    user_name: "string",
-    coinbase: "string",
-  },
-  { collection: "storage_management_owner", toJSON: { virtuals: true } }
+    {
+        user_id: { type: "string", index: true, unique: true },
+        user_name: "string",
+        coinbase: "string",
+    },
+    { collection: "storage_management_owner", toJSON: { virtuals: true } }
 );
+
+export const storageOwnerGroupSchema = new Schema<IStorageOwnerGroup>(
+    {
+        group_id: { type: "string", index: true, unique: true },
+    },
+    { collection: "storage_management_owner_group", toJSON: { virtuals: true } }
+)
 
 /**
  *
  */
 export const StorageOwnerModel: Model<IStorageOwner> =
-  mongoose.models[enums.ModelName.storageOwner] ??
-  model<IStorageOwner>(enums.ModelName.storageOwner, storageOwnerSchema);
+    mongoose.models[enums.ModelName.storageOwner] ??
+    model<IStorageOwner>(enums.ModelName.storageOwner, storageOwnerSchema);
